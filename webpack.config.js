c3c27f04a9d7ccac8abf1a584356d9896bd1005d
coldplayer <idhant123@gmail.com>
2017-06-27 20:07:49 +0530

    need to fix this

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,42 @@
+// var path = require('path');
+//
+// module.exports = {
+//   entry: './src/index.js',
+//   output: {
+//     filename: 'bundle.js',
+//     path: path.resolve(__dirname, 'dist')
+//   }
+// };
+
 var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    devServer: {
+        inline: true,
+        contentBase: './src',
+        port: 3000
+    },
+    devtool: 'cheap-module-eval-source-map',
+    entry: './src/index.js',
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loaders: ['babel-loader'],
+                exclude: /node_modules/
+            },
+            // {
+            //     test: /\.scss/,
+            //     loader: 'style-loader!css-loader!sass-loader'
+            // }
+        ]
+    },
+    output: {
+        filename: 'dist',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new webpack.optimize.OccurrenceOrderPlugin()
+    ]
 };
