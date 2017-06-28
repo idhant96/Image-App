b61688ad0ad19938df2dd3ec72436b8190b9cf91
coldplayer <idhant123@gmail.com>
2017-06-28 23:56:36 +0530

    added combining images reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,14 @@
 import { combineReducers } from 'redux';
 import UserReducer from './reducer-client';
 import ActiveReducer from './active-user-reducer';
+import Images from './reducer-players';
+import ActiveImage from './active-image';
 
 const allreducers = combineReducers({
   users:UserReducer,
-  activeUser: ActiveReducer
+  activeUser: ActiveReducer,
+  images: Images,
+  activeImage: ActiveImage
 });
 
 export default allreducers;
