0a70b93ed36ec581068dfac5a65c568e996299f4
coldplayer <idhant123@gmail.com>
2017-06-28 23:57:50 +0530

    added images reducer data

---
+++ src/reducers/reducer-players.js
@@ -0,0 +1,21 @@
+export default function(){
+  return [{
+    "id": 1,
+    "name": "Chris Martin",
+    "gender": "Male",
+    "Profession": "Singer",
+    "source": "https://www.eestickets.com/wp-content/uploads/2017/01/coldplay.jpg"
+  }, {
+    "id": 2,
+    "first_name": "cristiano-ronaldo",
+    "gender": "Male",
+    "Profession": "Footballer",
+    "source": "http://soccer-tricks.com/wp-content/uploads/2014/10/cristiano-ronaldo1.jpg"
+  }, {
+    "id": 3,
+    "name": "Tom Hanks",
+    "gender": "Male",
+    "Profession": "Actor",
+    "source": "https://images-na.ssl-images-amazon.com/images/M/MV5BMTQ2MjMwNDA3Nl5BMl5BanBnXkFtZTcwMTA2NDY3NQ@@._V1_UY317_CR2,0,214,317_AL_.jpg"
+  }]
+}
